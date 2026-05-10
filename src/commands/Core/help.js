de4737d8fa10b687a0141ccf4e0c8c2ccaff7bd8
Top1Speed_GT <gamerbrantleymcintyre6@gmail.com>
2026-05-10 10:29:50 -0400

    Update bug report button URL in help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -164,7 +164,7 @@ export async function createInitialHelpMenu(client) {
 
     const bugReportButton = new ButtonBuilder()
         .setLabel("Report Bug")
-        .setURL("https://www.youtube.com/")
+        .setURL("https://top1speedgt.github.io/")
         .setStyle(ButtonStyle.Link);
 
     const supportButton = new ButtonBuilder()
